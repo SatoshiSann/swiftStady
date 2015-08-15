56d06e3e7cb291021d6d078eb6f2277f23a449f2
S.M <wolf1003104@icloud.com>
2015-08-15 23:08:19 +0900

    練習用swift

---
+++ swiftTest.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+
+var str = "Hello, playground";
+
+var str2 = "Hello swift" //変数(変更できる)
+let letTest : Int = 3; //定数(変更できない)
+
+if (str != str2) {
+    str = str2;
+}
+var value = 1;
+
+for i in 4...10{
+    value += i;
+}
+
