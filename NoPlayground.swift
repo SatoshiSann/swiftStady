db5c9fd85433faec269beee815db0071f4e6a857
S.M <wolf1003104@icloud.com>
2015-08-15 23:25:59 +0900

    進捗

---
+++ NoPlayground.swift
@@ -0,0 +1,9 @@
+//
+//  NoPlayground.swift
+//  
+//
+//  Created by S.M. on 2015/08/15.
+//
+//
+
+import Foundation
---
+++ test/NoPlayground.swift
@@ -0,0 +1,12 @@
+//
+//  NoPlayground.swift
+//  
+//
+//  Created by S.M. on 2015/08/15.
+//
+//
+
+import Foundation
+
+let NEW = "逆にプレイグラウドがわからない";
+println(NEW);
